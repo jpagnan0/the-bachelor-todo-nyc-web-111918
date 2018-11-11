40da477fd8b23a61b6c0b8baac641275f2609c43
Adam Saland <a.jpagnano@gmail.com>
2018-11-11 01:44:23 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,10 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+   data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
